310b1f0d55c67470feee693a36fe499ed44ec555
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-07-15 13:57:42 +0300

    Added permissions for views

--- buyers/views.py
+++ buyers/views.py
@@ -1,18 +1,22 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Buyers, BuyerHistory, BuyerOffer
 from .serializers import BuyersSerializer, BuyerHistorySerializer, BuyerOfferSerializer
+from rest_framework import permissions
 
 
 class BuyersViewSet(ModelViewSet):
     queryset = Buyers.objects.all()
     serializer_class = BuyersSerializer
+    permission_classes = (permissions.AllowAny,)
 
 
 class BuyerHistoryViewSet(ModelViewSet):
     queryset = BuyerHistory.objects.all()
     serializer_class = BuyerHistorySerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class BuyerOfferViewSet(ModelViewSet):
     queryset = BuyerOffer.objects.all()
     serializer_class = BuyerOfferSerializer
+    permission_classes = (permissions.IsAuthenticated,)
--- suppliers/views.py
+++ suppliers/views.py
@@ -1,18 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Supplier, Suppliers, SupplierPromotion
 from .serializers import SuppliersSerializer, SupplierPromotionSerializer, SupplierSerializer
+from rest_framework import permissions
 
 
 class SuppliersViewSet(ModelViewSet):
     queryset = Suppliers.objects.all()
     serializer_class = SuppliersSerializer
+    permission_classes = (permissions.AllowAny,)
 
 
 class SupplierViewSet(ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
+    permission_classes = (permissions.AllowAny,)
 
 
 class SupplierPromotionViewSet(ModelViewSet):
     queryset = SupplierPromotion.objects.all()
     serializer_class = SupplierPromotionSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
