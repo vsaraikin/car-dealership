266baa401b4b358be852c97cad9aa4cd4682e559
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-07-16 13:49:24 +0300

    Added filtering, ordering and searching for DRF

--- buyers/views.py
+++ buyers/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from .models import Buyers, BuyerHistory, BuyerOffer
 from .serializers import BuyersSerializer, BuyerHistorySerializer, BuyerOfferSerializer
@@ -8,12 +10,20 @@ class BuyersViewSet(ModelViewSet):
     queryset = Buyers.objects.all()
     serializer_class = BuyersSerializer
     permission_classes = (permissions.AllowAny,)
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = '__all__'
+    search_fields = '__all__'
+    ordering_fields = '__all__'
 
 
 class BuyerHistoryViewSet(ModelViewSet):
     queryset = BuyerHistory.objects.all()
     serializer_class = BuyerHistorySerializer
     permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = '__all__'
+    search_fields = '__all__'
+    ordering_fields = '__all__'
 
 
 class BuyerOfferViewSet(ModelViewSet):
--- dealers/views.py
+++ dealers/views.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Dealer, Dealers, DealerPromotion, DealerToBuyer, SupplierToDealer
 from .serializers import DealerSerializer, DealersSerializer, DealerPromotionSerializer, DealerToBuyerSerializer, \
     SupplierToDealerSerializer
@@ -9,12 +11,20 @@ class DealersViewSet(ModelViewSet):
     queryset = Dealers.objects.all()
     serializer_class = DealersSerializer
     permission_classes = (permissions.AllowAny,)
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['id', 'name', 'location', 'balance']
+    search_fields = ['id', 'name', 'location', 'balance']
+    ordering_fields = ['id', 'name', 'location', 'balance']
 
 
 class DealerViewSet(ModelViewSet):
     queryset = Dealer.objects.all()
     serializer_class = DealerSerializer
     permission_classes = (permissions.AllowAny,)
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = '__all__'
+    search_fields = '__all__'
+    ordering_fields = '__all__'
 
 
 class DealerPromotionViewSet(ModelViewSet):
