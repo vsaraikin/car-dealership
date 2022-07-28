61c04e747f34bbeada59cbfaac8d91885149d229
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-07-28 13:41:06 +0300

    Removed comments

--- car_dealership/celery.py
+++ car_dealership/celery.py
@@ -6,8 +6,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_dealership.settings')
 
 app = Celery('car_dealership')
 
-# app.conf.broker_url = 'redis://localhost:6379/0'
-
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
