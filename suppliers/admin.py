3e3a6536e722eb561dc870c7206907e72581dbf2
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-07-28 13:44:48 +0300

    Added statistics for all counterparties

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Suppliers, Supplier, SupplierPromotion
+from .models import Suppliers, Supplier, SupplierPromotion, SupplierStatistic
 
 admin.site.register(Suppliers)
 admin.site.register(Supplier)
 admin.site.register(SupplierPromotion)
+admin.site.register(SupplierStatistic)
